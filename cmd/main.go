e644c26a4c534d6061a4c20cb5134814158e4f03
Egor <egoreek@MacBook-Pro-egor.local>
2025-06-14 13:44:32 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/jolypay/todo-app"
+	"github.com/jolypay/todo-app/package/handler"
+	"github.com/jolypay/todo-app/package/repository"
+	"github.com/jolypay/todo-app/package/service"
+	"github.com/spf13/viper"
+	"log"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("failed to start server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
---
+++ schema/000001_init_clean.up.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users
+(
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    username varchar(255) NOT NULL UNIQUE,
+    password_hash varchar(255) NOT NULL
+);
+
+CREATE TABLE todo_lists
+(
+    id serial PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    description varchar(255)
+);
+
+CREATE TABLE user_lists
+(
+    id serial PRIMARY KEY,
+    user_id int REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+    list_id int REFERENCES todo_lists(id) ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE todo_items
+(
+    id serial PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    description varchar(255),
+    done boolean NOT NULL DEFAULT false
+);
+
+CREATE TABLE lists_items
+(
+    id serial PRIMARY KEY,
+    items_id int REFERENCES todo_items(id) ON DELETE CASCADE NOT NULL,
+    list_id int REFERENCES todo_lists(id) ON DELETE CASCADE NOT NULL
+);
